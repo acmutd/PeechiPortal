9b27bc4dce8bdbea87bd84fe190c45f18c1750e7
jor-dango <jtan208295@gmail.com>
2025-04-02 03:22:56 -0500

    Added PlayerCheckIn page

---
+++ peechigamesportal/src/app/playercheckin/page.tsx
@@ -0,0 +1,83 @@
+'use client'
+
+import { ThemeProvider } from '@/components/theme-provider'
+import { Input } from '@/components/ui/input'
+import { AuthProvider } from '@/context/AuthProvider'
+import { collection, doc, getDoc, getDocs, query, updateDoc, where } from 'firebase/firestore'
+import React, { useState } from 'react'
+import { db } from '../firebase'
+
+function PlayerCheckIn() {
+  const [discInput, setDiscInput] = useState('');
+
+  async function handleCheckIn() {
+    const participantsRef = collection(db, "testParticipants");
+    const q = query(participantsRef, where("discordUsername", "==", discInput));
+    const qSnapshot = await getDocs(q);
+
+    // Get playercount to assign playernumber
+    let currentPlayerCount: number = -10; // -10 as a sentinel
+    const playerCountRef = doc(db, "metadata", "counter");
+    const docSnap = await getDoc(playerCountRef);
+    if (docSnap.exists()) {
+      currentPlayerCount = docSnap.data().nextnumber;
+    } else {
+      window.confirm("Could not verify next player number. Please try again.")
+      return;
+    }
+
+    let numDocsUpdated = 0;
+    qSnapshot.forEach((document) => {
+      try {
+        const docRef = doc(db, "testParticipants", document.id);
+        updateDoc(docRef, {
+          playernumber: currentPlayerCount
+        })
+        numDocsUpdated++;
+
+        // Increment playercount by 1 if update is successful
+        updateDoc(playerCountRef, {
+          nextnumber: currentPlayerCount + 1
+        })
+      }
+      catch (error) {
+        console.error(error);
+      }
+    })
+    if (numDocsUpdated !== 0) {
+      window.confirm("Successfully added. " + discInput + " is player " + currentPlayerCount)
+    }
+    else { // If no users were found with this username
+      window.confirm("User not found.")
+    }
+  }
+
+  return (
+    <AuthProvider>
+      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+        <div className='p-4 space-y-8'>
+          <h2 className='text-2xl font-bold'>
+            Check-In Players
+          </h2>
+          <div className=''>
+            <p>Enter the player's Discord username:</p>
+            <Input
+              className='w-[25rem] max-w-full'
+              value={discInput}
+              onChange={(e) => setDiscInput(e.target.value)}
+            />
+          </div>
+
+          <button
+            className='px-4 py-1 rounded-sm bg-slate-400 hover:bg-slate-500 active:bg-slate-500 text-black'
+            onClick={handleCheckIn}
+          >
+            Check In Player
+          </button>
+        </div>
+      </ThemeProvider>
+    </AuthProvider>
+  )
+}
+
+export default PlayerCheckIn
